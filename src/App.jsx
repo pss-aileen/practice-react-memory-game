399b40892f54b14aba8d5a1bb54382ffbf1ca904
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-08-20 11:32:14 +0900

    create cards array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import './App.css';
 
+const cards = [
+  {
+    id: 1,
+    name: 'A',
+    isMatched: false,
+  },
+  {
+    id: 2,
+    name: 'B',
+    isMatched: false,
+  },
+  {
+    id: 3,
+    name: 'C',
+    isMatched: false,
+  },
+  {
+    id: 4,
+    name: 'D',
+    isMatched: false,
+  },
+];
+
 function App() {
   return (
     <>
